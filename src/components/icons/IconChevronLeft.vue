982d1efafae568bb8e5d520f671ec3563468bf6e
lazizbekshodmonov <lazizbekshodmonov30@gmail.com>
2024-02-27 21:18:43 +0500

    fix bug

--- src/components/icons/IconChevronLeft.vue
+++ src/components/icons/IconChevronLeft.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <svg width="1em" height="1en" viewBox="0 0 24 24" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
     <path d="M15 18L9 12L15 6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>
 </template>
