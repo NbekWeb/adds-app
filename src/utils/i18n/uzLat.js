477efc5285d19c102f7250f57b19b3653472e634
lazizbekshodmonov <lazizbekshodmonov30@gmail.com>
2024-02-28 15:45:35 +0500

    add localise messages for menu

--- src/utils/i18n/uzLat.js
+++ src/utils/i18n/uzLat.js
@@ -2,7 +2,11 @@ const uzLat = {
   LoginView: 'Tizimga kirish',
   DashboardView: 'Asosiy board',
   DashboardNotFond: 'Sahifa topilmadi',
-  DashboardListView: "Ro'yxat"
+  DashboardListView: 'Asosiy sahifa',
+  AnnouncementsView: "E'lonlar",
+  ReportView: "E'lon bo'yicha hisobot",
+  AudienceView: 'Auditoriya',
+  BillingView: "Billing va to'lovlar"
 }
 
 export default uzLat
