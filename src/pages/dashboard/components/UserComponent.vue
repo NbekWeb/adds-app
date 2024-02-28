dae92147c4598dd0393da5546a01562d774622c4
lazizbekshodmonov <lazizbekshodmonov30@gmail.com>
2024-02-28 15:35:48 +0500

    add log out

--- src/pages/dashboard/components/UserComponent.vue
+++ src/pages/dashboard/components/UserComponent.vue
@@ -1,83 +1,91 @@
 <script setup>
-import IconImageEmpty from "@/components/icons/IconImageEmpty.vue";
+import IconImageEmpty from '@/components/icons/IconImageEmpty.vue'
+import useUser from '@/store/user.pinia.js'
+import { storeToRefs } from 'pinia'
 
-const {avatar,size} = defineProps({
-  avatar:{
-    type: [String,null],
+const { avatar, size } = defineProps({
+  avatar: {
+    type: [Boolean, null],
     default: null
   },
-  size:{
+  size: {
     type: String,
-    default:'large'
+    default: 'large'
   }
 })
+
+const userPinia = useUser()
+const { user } = storeToRefs(userPinia)
 </script>
 
 <template>
   <div class="user">
     <div class="avatar">
       <template v-if="avatar">
-        <img src="@/assets/images/for_remove/avatar.png" alt="">
+        <img src="@/assets/images/for_remove/avatar.png" alt="" />
       </template>
       <template v-else>
         <div class="no-avatar">
-          <icon-image-empty/>
+          <icon-image-empty />
         </div>
       </template>
     </div>
     <div class="name">
-      <b>Dilshod Mirsoatov</b>
-      <p class="m-0">ID: 19200395</p>
+      <b
+        >{{ user?.firstName ? user?.firstName : 'Firstname' }}
+        {{ user?.lastName ? user?.lastName : 'Lastname' }}</b
+      >
+      <p class="m-0">ID: {{ user?.id }}</p>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-@import "@/assets/styles/variable";
-  .user{
-    width: 100%;
+@import '@/assets/styles/variable';
+.user {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid rgba($muted, 0.3);
+  padding: 0.4rem 0.8rem;
+  border-radius: 4px;
+  .avatar {
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+    border: 1px solid rgba($muted, 0.3);
     display: flex;
-    align-items: center;
     justify-content: center;
-    border: 1px solid rgba($muted,.3);
-    padding: .4rem 0.8rem;
-    border-radius: 4px;
-    .avatar{
-      width: 36px;
-      height: 36px;
-      border-radius: 50%;
-      border: 1px solid rgba($muted,.3);
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      color: $muted;
-      background-color: rgba($muted,.2);
-      font-size: 18px;
-      overflow: hidden;
-      svg{
-        vertical-align: -2px;
-      }
-      img{
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-      }
+    align-items: center;
+    color: $muted;
+    background-color: rgba($muted, 0.2);
+    font-size: 18px;
+    overflow: hidden;
+    svg {
+      vertical-align: -2px;
     }
-    .name{
-      width: calc(100% - 36px);
-      padding-left: .5rem;
-      font-size: 12px;
-      height: 28px;
-      display: -webkit-box;
-      -webkit-line-clamp: 2;
-      -webkit-box-orient: vertical;
-      overflow: hidden;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
     }
   }
-.collapsed .user{
+  .name {
+    width: calc(100% - 36px);
+    padding-left: 0.5rem;
+    font-size: 12px;
+    height: 28px;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+  }
+}
+.collapsed .user {
   padding: 0;
   border: none;
-  .name{
+  .name {
     display: none;
   }
 }
