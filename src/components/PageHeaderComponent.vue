17bc81da09442770c514cb35841dfb070f617469
lazizbekshodmonov <lazizbekshodmonov30@gmail.com>
2024-03-13 18:25:28 +0500

    add header component for all page

---
+++ src/components/PageHeaderComponent.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+  title: String
+})
+</script>
+
+<template>
+  <div class="page-header">
+    <h1 class="title">{{ title }}</h1>
+    <slot name="actions"></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.page-header {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  .title {
+    margin: auto 0;
+    font-size: 18px;
+    font-weight: bold;
+  }
+}
+</style>
