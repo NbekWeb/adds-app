c77ebedb2e6010d015a16f292aed4a312513fbe4
lazizbekshodmonov <lazizbekshodmonov30@gmail.com>
2024-05-10 12:09:21 +0500

    add configuration info to order item

--- src/pages/dashboard/order/[id]/components/ConfigurationComponent.vue
+++ src/pages/dashboard/order/[id]/components/ConfigurationComponent.vue
@@ -1,5 +1,6 @@
 <script setup>
 import dayjs from 'dayjs'
+import BoardConfigurationTimeComponent from '@/components/BoardConfigurationTimeComponent.vue'
 const props = defineProps({
   configuration: [Object, null],
   startDate: String
@@ -10,6 +11,20 @@ const props = defineProps({
   <div class="configuration pb-1 mb-3 pb-3">
     <template v-if="configuration">
       <h2>{{ configuration?.name }}</h2>
+      <div class="flex w-full">
+        <board-configuration-time-component
+          :configuration="configuration"
+          time-type="liveTime"
+        /><board-configuration-time-component
+          class="mx-4"
+          :configuration="configuration"
+          time-type="topTime"
+        /><board-configuration-time-component
+          :configuration="configuration"
+          time-type="pinTime"
+        />
+      </div>
+
       <div class="mt-2 flex justify-between align-center">
         <h4 class="m-0">{{ $t('THE_TIME_OF_PUBLICATION') }}</h4>
         <p class="m-0">{{ dayjs(startDate).format('DD.MM.YYYY, HH:mm') }}</p>
