d136eb24b3f11c589dabca0d47e95fcaa84ccad1
Diyorbek <diyorbekjoraqulov8@gmail.com>
2024-05-22 15:41:21 +0300

    Some changes

---
+++ src/pages/dashboard/kiosk-board/[id]/KioskBoardOrderComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {onMounted} from "vue";
+import usePost from "@/store/post.pinia.js";
+import {storeToRefs} from "pinia";
+
+const storePost = usePost()
+
+const { posts } = storeToRefs(storePost)
+
+onMounted(() => {
+  storePost.getAllKioskPosts(0)
+})
+</script>
+
+<template>
+  <div>
+    {{ posts }}
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
